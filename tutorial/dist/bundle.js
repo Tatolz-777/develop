cae213e34c5f2925be632f919cfd7a7327116f8d
Tatolz <kosn.ten.7727@gmail.com>
2024-09-01 15:22:23 +0900

    main

--- tutorial/dist/bundle.js
+++ tutorial/dist/bundle.js
@@ -14,20 +14,54 @@
 /*!**********************!*\
   !*** ./src/index.ts ***!
   \**********************/
-/***/ (() => {
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\n\n\n//# sourceURL=webpack://tutorial/./src/index.ts?");
+eval("\nvar __importDefault = (this && this.__importDefault) || function (mod) {\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\n};\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nconst saySomething_1 = __importDefault(__webpack_require__(/*! ./saySomething */ \"./src/saySomething.ts\"));\nconst root = document.getElementById(\"root\");\nconst saySomething = new saySomething_1.default(\"ようこそ\");\nsaySomething.sayText(root);\n\n\n//# sourceURL=webpack://tutorial/./src/index.ts?");
+
+/***/ }),
+
+/***/ "./src/saySomething.ts":
+/*!*****************************!*\
+  !*** ./src/saySomething.ts ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, exports) => {
+
+eval("\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\nclass SaySomething {\n    constructor(message) {\n        this.message = message;\n    }\n    sayText(elem) {\n        if (elem) {\n            elem.innerText = this.message;\n        }\n    }\n}\nexports[\"default\"] = SaySomething;\n\n\n//# sourceURL=webpack://tutorial/./src/saySomething.ts?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
 /******/ 	
 /******/ })()
 ;
--- tutorial/src/index.ts
+++ tutorial/src/index.ts
@@ -2,5 +2,5 @@ import SaySomething from "./saySomething";
 
 const root:HTMLElement | null = document.getElementById("root");
 
-const saySomething = new SaySomething("Hello World");
+const saySomething = new SaySomething("ようこそ");
 saySomething.sayText(root);
